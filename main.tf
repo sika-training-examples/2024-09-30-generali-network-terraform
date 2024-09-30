15d05995315874ae24a478223a149951bf562874
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-30 11:36:16 +0200

    feat: Setup ondrejsika subnet

--- main.tf
+++ main.tf
@@ -9,3 +9,14 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "main" {
+  for_each = {
+    "ondrejsika" = ["10.0.1.0/24"]
+  }
+
+  name                 = each.key
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = "example-generali-network"
+  address_prefixes     = each.value
+}
