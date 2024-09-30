b56b10dc9c8bcaf03005504ed5701dee8ae5cbf3
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-30 13:47:16 +0200

    refactor: Move example-generali-network resources to network module

--- main.tf
+++ main.tf
@@ -1,25 +1,13 @@
-resource "azurerm_resource_group" "main" {
-  name     = "example-generali-network"
-  location = "westeurope"
-}
+module "example_generali_network" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//network?ref=master"
 
-resource "azurerm_virtual_network" "main" {
-  name                = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.0.0.0/16"]
-}
+  name          = "example-generali-network"
+  address_space = ["10.0.0.0/16"]
 
-resource "azurerm_subnet" "main" {
-  for_each = {
+  subnets = {
     "ondrejsika" = ["10.0.1.0/24"]
     "foo"        = ["10.0.2.0/24"]
   }
-
-  name                 = each.key
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = "example-generali-network"
-  address_prefixes     = each.value
 }
 
 module "hello_world_network" {
