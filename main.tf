297123e99d6d0b6e29a2365fce6a87ea411da59a
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-30 13:37:04 +0200

    feat: Create network hello-world

--- main.tf
+++ main.tf
@@ -21,3 +21,15 @@ resource "azurerm_subnet" "main" {
   virtual_network_name = "example-generali-network"
   address_prefixes     = each.value
 }
+
+module "hello_world_network" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//network?ref=master"
+
+  name          = "hello-world"
+  address_space = ["10.0.0.0/16"]
+
+  subnets = {
+    "foo" = ["10.0.1.0/24"]
+    "bar" = ["10.0.2.0/24"]
+  }
+}
